ec310b43f57d18c2d133b22b48217ed5839dad8d
jacqueshovine <hovine.jacques@gmail.com>
2022-02-03 23:32:30 +0100

    add style to the create articles page

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,31 +2,44 @@
 
 @section('body')
 
-<p>Create article</p>
+<main class="container">
 
-<!-- Appelle la méthode store -->
-<form method="POST" action="/articles">
-  @csrf
-  <div class="mb-3">
-    <label class="form-label" for="title">Title</label>
-    <input class="form-control" type="text" name="title" id="title"/>
-  </div>
+    <div class="col text-start mt-4">
+        <h1>Create article</h1>
+    </div>
 
-  <div class="mb-3">
-    <label class="form-label" for="body">Body</label>
-    <textarea class="form-control" name="body" id="body"></textarea>
-  </div>
+    <!-- Appelle la méthode store -->
+    <form method="POST" action="/articles">
+    @csrf
+    <div class="mb-3">
+        <label class="form-label" for="title">Title</label>
+        <input class="form-control" type="text" name="title" id="title"/>
+    </div>
 
-  <input class="btn btn-primary" type="submit"/>
+    <div class="mb-3">
+        <label class="form-label" for="body">Body</label>
+        <textarea class="form-control" name="body" id="body"></textarea>
+    </div>
 
-  @error('title')
-    <div class="alert alert-danger">{{ $message }}</div>
-  @enderror
+    <div>
+        <a href="/articles" class="btn" style="background-color: coral; color:white;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>
+            Back to list</a>
+        <input class="btn" type="submit" style="background-color: coral; color:white;"/>
+    </div>
 
-  @error('body')
-    <div class="alert alert-danger">{{ $message }}</div>
-  @enderror
+    @error('title')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
 
-</form>
+    @error('body')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+
+    </form>
+
+</main>
 
 @endsection
